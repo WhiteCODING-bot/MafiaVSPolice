896591fa1c5278e66582e7309aa6bec17b91e733
ImFabi0 <ImFabi0@JESUS>
2023-04-04 13:46:16 +0200

    Add project files.

---
+++ Random_Stuff/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace randomMap
+{
+    class map
+    {
+        map()
+        {
+
+        }
+        public static void main(String[] args)
+        {
+            /*
+             TO DO:
+                -> Gang 1
+                -> Gang 2
+                -> Polizia
+                -> Matrice 5 x 5
+             */
+        }
+    }
+}
