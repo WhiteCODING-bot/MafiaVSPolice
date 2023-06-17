81cd49b126fd3c4cb6028dd029fed1a67b72a123
ImFabi0 <mmfabiop.hyspa@gmail.com>
2023-06-17 15:33:29 +0200

    Add Ambrogio for manage Mafia and Police

---
+++ Random_Stuff/Multi_Stuff/Ambrogio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Random_Stuff;
+
+
+namespace Random_Stuff.Multi_Stuff
+{
+    /*
+     * Parametri:
+     *  ->Matrice
+     *  ->Posizioni:
+     *      ->Mafia
+     *      ->Polizia
+     *      
+     */
+
+    internal class Ambrogio
+    {
+
+    }
+}
