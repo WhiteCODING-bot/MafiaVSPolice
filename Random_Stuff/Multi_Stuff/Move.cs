c6ea462ad67e6fdc8a43e2b40d25734fc3114b61
Hyspa_port <hyspa.fabio@gmail.com>
2023-08-26 11:26:40 +0200

    Re-do "moveThroughMap" but it doesn't work so I must fix it

--- Random_Stuff/Multi_Stuff/Move.cs
+++ Random_Stuff/Multi_Stuff/Move.cs
@@ -13,6 +13,7 @@ namespace Random_Stuff
         private Map map = new Map();
         public Move() { }
 
+        
         public int isOk(int[] pos)
         {
             int x = pos[0];
@@ -26,20 +27,21 @@ namespace Random_Stuff
             }
             switch (matrix.GetValue(x, y))
             {
-                case 0:
+                case null:
                     //La cella è libera
                     return 0;
-                case 1:
+                case Gang:
                     //Nella cella c'è una gang
                     return 1;
-                case 2:
+                case Police:
                     //Nella cella c'è la polizia
                     return 2;
             }
             return 93;
         }
 
-        public int isOk(int x,int y)
+
+        public int isOk (int x,int y)
         {
             object[,] matrix = randomMap.Map.getMatrix();
             if( matrix == null)
@@ -50,40 +52,24 @@ namespace Random_Stuff
             switch(matrix.GetValue(x,y)){
                 case 0:
                     return 0;
-                case 1:
+                case Gang:
                     return 1;
-                case 2:
+                case Police:
                     return 2;
             }
             return 3;
         }
 
-        public void moveThroughMap(int[] last_pos,int team)
+        public void moveThroughMap(int[] last_pos,object? team)
         {
-            Police police = new Police();
-            int x = last_pos[0];
-            int y = last_pos[1];
-            if(team == 2)
+            Map game_map = new Map();
+            if (isOk(last_pos) == 0)
             {
-                if (isOk(x, y + 1) == 0)
-                {
-                    object[,] matrix = randomMap.Map.getMatrix();
-                    matrix[x, y] = 0;
-                    matrix[x + 1, y] = team;
-                    police.setLastPos(police.whereIs());
-                }
-                else
-                {
-                    Console.Write("Cella occupata");
-                }
-
+                object[,] map = Map.getMatrix();
+                map[last_pos[0]+1, last_pos[1]+1] = team;
+                game_map.updateMatrix(map);
             }
-            else {
-                object[,] matrix = randomMap.Map.getMatrix();
-                matrix[x, y] = 0;
-                matrix[x + 1, y] = team;
 
-            }
         }
 
 
--- Random_Stuff/Program.cs
+++ Random_Stuff/Program.cs
@@ -22,34 +22,35 @@ namespace randomMap
     class Map
     {
         private static object[,] map_game = new object[10,10];
-        Random random = new Random();
-        public Map(){}
-        public void initPos_Test()
-        {
-
-            Move move = new Move();
-            int[] pos = { 0, 0, 2, 0};
-
-            map_game[pos[0], pos[1]] = 1;
-            map_game[pos[2], pos[3]] = 2;
-
+        public Map(){
         }
 
-
-        public void initPos()
-        {
-            Move move = new Move();
-            int[] pos = { random.Next(0, map_game.GetLength(0)),
-                random.Next(0, map_game.GetLength(0)),
-                random.Next(0, map_game.GetLength(0)),
-                random.Next(0, map_game.GetLength(0))};
-
-            map_game[pos[0], pos[1]] = 1;
-            map_game[pos[2], pos[3]] = 2;
-        }
         public static object[,] getMatrix()
         {
             return map_game;
+        }
+
+        public void updateMatrix(object[,] u_map)
+        {
+            for (int i = 0; i < u_map.GetLength(0); i++)
+            {
+                for(int j = 0; j < u_map.GetLength(1); j++)
+                {
+                    map_game[i,j] = u_map[i,j];
+                }
+            }
+        }
+
+        public static void initMatrix()
+        {
+            object[,] map = map_game;
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    map[i, j] = null;
+                }
+            }
         }
 
         public void show_Stuff(Map map)
@@ -58,52 +59,53 @@ namespace randomMap
             {
                 for (int j = 0; j < randomMap.Map.map_game.GetLength(1); j++)
                 {
+                    Console.Write("| ");
                     switch(randomMap.Map.map_game[i, j])
                     {
-                        case 0:
+                        case null:
                             Console.Write("X ");
                             break;
-                        case 1:
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write("G ");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            break;
-                        case 2:
+                        
+                        case Police:
                             Console.ForegroundColor = ConsoleColor.Green;
                             Console.Write("P ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                        
+                        case Gang:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("G ");
                             Console.ForegroundColor = ConsoleColor.White;
                             break;
 
                     }
                 }
-                Console.Write("\n");
+                Console.Write("|\n");
             }
         }
         public static void Main(String[] args)
         {
             Map map = new Map();
-
             Police police = new Police();
-
+            Gang gang = new Gang();
             Move move = new Move();
+            Random random = new Random();
 
-            Boss boss1 = new Boss("Fabio");
+            initMatrix();
+            object[,] game_map = Map.getMatrix();
 
-            Gang gang1 = new Gang(101, boss1);
+            game_map[0,0] = gang;
+            game_map[random.Next(game_map.GetLength(1)), random.Next(game_map.GetLength(1))] = police;
 
-            int x = randomMap.Map.map_game.GetLength(0)-1;
-            int y = randomMap.Map.map_game.GetLength(1)-1;
 
-            map.initPos_Test();
+            map.show_Stuff(map);
+            int[] tmp = gang.whereIs();
+            Console.WriteLine(tmp[0] + " " + tmp[1]);
+            move.moveThroughMap(tmp,gang);
+
+            Console.WriteLine("\n\n");
             map.show_Stuff(map);
 
-            Console.WriteLine("\n");
-            
-            move.moveThroughMap(gang1.whereIs(), 1);
-            move.moveThroughMap(police.whereIs(), 2);
-            map.show_Stuff(map);
-
-            Console.WriteLine("\n");
         }
     }
 }
